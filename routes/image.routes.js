0e350e4da172d3217d5d51a7758c61cf091ffff6
kruti2107 <chodwadiakruti@gmail.com>
2019-04-19 11:45:21 -0700

    Add multiple image uploading

--- routes/image.routes.js
+++ routes/image.routes.js
@@ -18,7 +18,7 @@ var storage=multer.diskStorage({
         width:100
     }
 });
-
+//Single image uploading
 var upload = multer({storage:storage});
 router.post('/',upload.single('image'), (req,res,next)=>{
     req.body.image= req.file.filename;
@@ -33,5 +33,23 @@ router.post('/',upload.single('image'), (req,res,next)=>{
         }
     })
 });
-
+//multiple image uploading
+// var upload = multer({storage:storage}).array('image',10);
+// router.post('/',upload, (req,res,next)=>{
+//     var pictures=[];
+//     for(let picture of req.files){
+//         pictures.push(picture.filename);
+//     }
+//     req.body.image=JSON.stringify(pictures);
+//     post(req.body,(err,result)=>{
+//         if(err){
+//             res.statusCode=400;
+//             res.json(err);
+//         }
+//         else{
+//             res.statusCode=200;
+//             res.json(result);
+//         }
+//     })
+// });
 module.exports=router;
