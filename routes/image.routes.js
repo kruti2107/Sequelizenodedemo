533abb657a6ff4837b0e79660f4e5c83d6e4f09b
kruti2107 <chodwadiakruti@gmail.com>
2019-04-19 11:43:11 -0700

    Add single image uploading

--- routes/image.routes.js
+++ routes/image.routes.js
@@ -19,13 +19,9 @@ var storage=multer.diskStorage({
     }
 });
 
-var upload = multer({storage:storage}).array('image',10);
-router.post('/',upload, (req,res,next)=>{
-    var pictures=[];
-    for(let picture of req.files){
-        pictures.push(picture.filename);
-    }
-    req.body.image=JSON.stringify(pictures);
+var upload = multer({storage:storage});
+router.post('/',upload.single('image'), (req,res,next)=>{
+    req.body.image= req.file.filename;
     post(req.body,(err,result)=>{
         if(err){
             res.statusCode=400;
